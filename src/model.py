9e27259e6cb82fc256022bed8988c92f321a7bb1
Filip Zurek <filipzurek.fake@gmail.com>
2025-12-06 15:41:22 +0100

    Adding comments

--- src/model.py
+++ src/model.py
@@ -2,15 +2,24 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense
 
 def build_cnn():
+    """
+    Defines the CNN architecture used for both experiments.
+    """
     model = Sequential([
+        # First convolutional block
         Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
         MaxPool2D(2, 2),
+
+        # Second convolutional block
         Conv2D(64, (3, 3), activation='relu'),
+
+        # Flatten and dense layers for classification
         Flatten(),
         Dense(128, activation='relu'),
         Dense(10, activation='softmax')
     ])
 
+    # Compile with Adam optimizer and categorical crossentropy loss
     model.compile(
         optimizer='adam',
         loss='categorical_crossentropy',
